3802a3d84c0b6b9fb66772df90c026e765fe926c
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-19 22:07:40 +0530

    Frontend work

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "background-primary": "#468499",
+        "background-secondary": "#0f2229",
+        "text-color-primary": "#29160F",
+      },
+    },
+  },
+  plugins: [],
+};
