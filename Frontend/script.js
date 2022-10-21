3080be262a2bb26c11335928824f7e24a2f7ffa4
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-21 23:00:17 +0530

    js added

---
+++ Frontend/script.js
@@ -0,0 +1,23 @@
+const generate_btn = document.querySelector("#generate-btn");
+const close_btn = document.querySelector("#close-btn");
+const result = document.querySelector("#result_div");
+const result_text = document.querySelector("#result_text");
+const drop_down_list = document.querySelectorAll("#drop-down-list").value();
+
+generate_btn.addEventListener("click", () => {
+  if (result.classList.contains("hidden")) {
+    setTimeout(() => {
+      result.classList.remove("hidden");
+    }, 50);
+  }
+});
+
+close_btn.addEventListener("click", () => {
+  if (!result.classList.contains("hidden")) {
+    setTimeout(() => {
+      result.classList.add("hidden");
+    }, 50);
+  }
+});
+
+console.log(drop_down_list);
