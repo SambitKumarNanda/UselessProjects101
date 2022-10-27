d872246776b93080e218ecf29e0a98094953d9f2
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-27 19:22:40 +0530

    added python hello world msg

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,7 +2,7 @@ const generate_btn = document.querySelector("#generate-btn");
 const close_btn = document.querySelector("#close-btn");
 const result = document.querySelector("#result_div");
 const result_text = document.querySelector("#result_text");
-const drop_down_list = document.querySelectorAll("#drop-down-list").value();
+const drop_down_list = document.querySelectorAll("#drop-down-list");
 
 generate_btn.addEventListener("click", () => {
   if (result.classList.contains("hidden")) {
