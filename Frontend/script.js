80f5ba644dd4f34e3b4cd1a1330f9d0222afcf76
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-30 23:22:48 +0530

    Added Hello world syntaxes for python,javascript and c language

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,9 +2,20 @@ const generate_btn = document.querySelector("#generate-btn");
 const close_btn = document.querySelector("#close-btn");
 const result = document.querySelector("#result_div");
 const result_text = document.querySelector("#result_text");
-const drop_down_list = document.querySelectorAll("#drop-down-list");
 
 generate_btn.addEventListener("click", () => {
+
+    let selectedValue = document.getElementById("drop-down-list").value;
+    console.log(selectedValue);
+    switch (selectedValue){
+        case 'python': result_text.innerHTML=`print("Hello World")`
+        break;
+        case 'javascript': result_text.innerHTML=`console.log("Hello World")`
+        break;
+        case 'c': result_text.innerHTML=`printf("Hello World")`
+        break;
+    }
+
   if (result.classList.contains("hidden")) {
     setTimeout(() => {
       result.classList.remove("hidden");
